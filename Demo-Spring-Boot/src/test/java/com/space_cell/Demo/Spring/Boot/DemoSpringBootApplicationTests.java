5cec32c12c04eb7dfa392f46a502c38e395a38f9
Kirill Kozlov <space_cell@proton.me>
2024-06-05 11:48:02 +0500

    Init App

---
+++ Demo-Spring-Boot/src/test/java/com/space_cell/Demo/Spring/Boot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space_cell.Demo.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
